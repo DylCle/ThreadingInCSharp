ca437b905ca7fe1b15f564cd7c9e2838d24263eb
Dylan Cleveland <newrussianguy12@gmail.com>
2023-04-26 12:14:34 -0300

    completed threading

--- ThreadingUpdateUI/Form1.cs
+++ ThreadingUpdateUI/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,20 +19,69 @@ namespace ThreadingUpdateUI
             InitializeComponent();
         }
 
+        private List<Label> threadLabels = new List<Label>();
       
         private void btnGo_Click(object sender, EventArgs e)
         {
+            if(threadLabels.Count < 100)
+            {
+                //reading from UI
+                int.TryParse(textBox1.Text, out int iteration);
+                int.TryParse(textBox2.Text, out int sleepDuration);
 
+                //Setting up label
+                Label l = new Label();
+                l.Text = DateTime.Now.ToLongTimeString();
+                threadLabels.Add(l);
+
+                l.Top = (panel1.Controls.Count) * l.Height + 20;
+                l.Left = 5;
+
+                //Adding the label we created so its on the UI
+                this.panel1.Controls.Add(l);
+                
+                //Creating actual thread
+                Task.Factory.StartNew(() =>
+                {
+                    for (int i = 0; i <= iteration; i++)
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            l.Text = i.ToString();
+                        });
+
+                        Thread.Sleep(sleepDuration);
+
+                    }
+                });
+            }
         }
 
         private void btnGoNoThread_Click(object sender, EventArgs e)
         {
+            int.TryParse(textBox1.Text, out int iteration);
+            int.TryParse(textBox2.Text, out int sleepDuration);
 
+            btnGoNoThread.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+
+            for (int i = 0; i <= iteration; i++)
+            {
+                lblCounter.Text = i.ToString();
+                lblCounter.Refresh();
+                Thread.Sleep(sleepDuration);
+            }
+
+            btnGoNoThread.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             //We need to kill any running threads
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
             
         }
     }
